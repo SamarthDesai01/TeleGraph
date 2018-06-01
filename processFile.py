d850db57cfef3bd9804262993214686cc6151d1c
SamarthDesai01 <37949934+SamarthDesai01@users.noreply.github.com>
2018-05-31 21:24:24 -0500

    Fixed Issue when encountering deleted accounts

--- processFile.py
+++ processFile.py
@@ -15,55 +15,60 @@ totalMessageCount = 0
 mostRecentMessage = {0:0}
 
 for currentLine in file.readlines(): #Loop through every message in our specified file
-    totalMessageCount+=1
-    mediaFlag = 0
-    
     #Load the entire JSON object into a python dictionary 
     messageData = json.loads(currentLine)
-    #Data of only the sender of the current message, includes peer_id, name, and phone number 
-    currentSenderData = messageData['from']
-    
-    #Correctly set the first message as the most recent message to set an intial date correctly
-    if firstRunFlag == 1:
-        mostRecentMessage = {currentSenderData['peer_id']:messageData['date']}
-        firstRunFlag = 0
 
-    #Code used to check if a message was sent or a photo/file
-    if 'text' not in messageData: 
-        if 'media' in messageData:
-            mediaDict = messageData['media'] #Store the object containing info about the media sent 
-            if 'caption' in mediaDict: #Found a photo
-                messageText = messageData['media']['caption'] #Pass the caption 
-                mediaFlag = 1
-            elif mediaDict['type'] == 'document': #Found a file
-                messageText = 'Document' #Pass in a blank string as no message was sent with it 
-                mediaFlag = 2 
+    if messageData["event"] == "message":
+        totalMessageCount+=1
+        mediaFlag = 0
+        
+        #Data of only the sender of the current message, includes peer_id, name, and phone number 
+        currentSenderData = messageData['from']
+        
+        #Correctly set the first message as the most recent message to set an intial date correctly
+        if firstRunFlag == 1:
+            mostRecentMessage = {currentSenderData['peer_id']:messageData['date']}
+            firstRunFlag = 0
+
+        #Code used to check if a message was sent or a photo/file
+        if 'text' not in messageData: 
+            if 'media' in messageData:
+                mediaDict = messageData['media'] #Store the object containing info about the media sent 
+                if 'caption' in mediaDict: #Found a photo
+                    messageText = messageData['media']['caption'] #Pass the caption 
+                    mediaFlag = 1
+                elif mediaDict['type'] == 'document': #Found a file
+                    messageText = 'Document' #Pass in a blank string as no message was sent with it 
+                    mediaFlag = 2 
+            else:
+                messageText = ''
+        else: #Encountered a normal message 
+            messageText = messageData['text']
+            if 'media' in messageData: #Links are unique in that they include both text and media fields 
+                mediaDict = messageData['media']
+                if 'url' in mediaDict: #Double check that it's a link 
+                    messageText = '' #ignoring links sent so they don't skew word count 
+                    mediaFlag = 3
+
+        currID = messageData['from']['peer_id']
+        if 'first_name' in currentSenderData: #set currSender as the sender's full name if available
+            currSender = messageData['from']['first_name'] + " " + messageData['from']['last_name']
         else:
-            messageText = ''
-    else: #Encountered a normal message 
-        messageText = messageData['text']
-        if 'media' in messageData: #Links are unique in that they include both text and media fields 
-            mediaDict = messageData['media']
-            if 'url' in mediaDict: #Double check that it's a link 
-                messageText = '' #ignoring links sent so they don't skew word count 
-                mediaFlag = 3
-    
-    currSender = messageData['from']['first_name'] + " " + messageData['from']['last_name']
-    currID = messageData['from']['peer_id']
-    
-    #Declare a new user if they aren't already in userDict, {ID : UserData()}
-    if currID not in userDict: 
-        userDict[currID] = user.UserData(currID, currSender)
-    
-    #Update all relevant info about the current sender of this message 
-    userDict[currID].updateData(messageData, messageText, mediaFlag, checkWordCount, mostRecentMessage)
-    
-    #Run search methods for the current message to see if matches parameters set in start.py 
-    if hourRange[0] <= hourRange[1]: #Only look for messages if a valid hour range was provided 
-        userDict[currID].findMessage(messageData, messageText, hourRange, keyWords)
-    
-    #Store the time and user of the message just processed exp:(12345676: UNIX TIME CODE)
-    mostRecentMessage = {currID:messageData['date']}
+            currentSender = currentSenderData['print_name'] + str(currID)
+
+        #Declare a new user if they aren't already in userDict, {ID : UserData()}
+        if currID not in userDict: 
+            userDict[currID] = user.UserData(currID, currSender)
+        
+        #Update all relevant info about the current sender of this message 
+        userDict[currID].updateData(messageData, messageText, mediaFlag, checkWordCount, mostRecentMessage)
+        
+        #Run search methods for the current message to see if matches parameters set in start.py 
+        if hourRange[0] <= hourRange[1]: #Only look for messages if a valid hour range was provided 
+            userDict[currID].findMessage(messageData, messageText, hourRange, keyWords)
+        
+        #Store the time and user of the message just processed exp:(12345676: UNIX TIME CODE)
+        mostRecentMessage = {currID:messageData['date']}
 
 #Remove any users that we don't want to graph. These users were specified in start.py 
 for peerID in ignoreByPeerID:
@@ -87,4 +92,3 @@ for user in userDict:
 print("\n" + "Total Messages Processed: " + str(totalMessageCount) + "\n")
 
 import graph
-graph
